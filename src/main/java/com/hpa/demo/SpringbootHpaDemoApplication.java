e39c7c9a30e1e9eebf64a1872c60ddb80d134e95
ravi suryawanshi <ravisuryawanshi87@gmail.com>
2026-01-02 10:59:26 +0530

    Initial Spring Boot application

---
+++ src/main/java/com/hpa/demo/SpringbootHpaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHpaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootHpaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hpa/demo/SpringbootHpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHpaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
